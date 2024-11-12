16809acff7fd5e0baa2c7551b644e034c99a3916
brendobrito2002 <brendoplayer@gmail.com>
2024-11-12 18:34:49 -0300

    finalizacao parte1 da atvidade1

--- parte1.py
+++ parte1.py
@@ -110,3 +110,69 @@ print("16)c) ", n[0], n[1])
 print("16)d) ", n[:, 3])
 print("16)e) ", n[0, -3:], n[1, -3:])
 print("16)f) ", n[:, -5:])
+
+# 17) Crie 1 array de 21 elementos usando o comando arange e reshape em uma mesma linha de comando, 
+# depois de criado altere o primeiro elemento para 51
+
+o = np.arange(21).reshape(3, 7)
+print("17) ", o)
+o[0, 0] = 51
+print("17) ", o)
+
+# 18) No array anterior (3,7) altere apenas as linhas 2 e 3 para que os 3 primeiros elementos de cada seja 0.
+
+p = o.copy()
+p[1:, :3] = 0
+print("18) ", p)
+
+# 19) Crie duas listas e apresente em uma nova lista:
+# a. A soma os elementos da lista
+# b. A multiplicação dos elementos dessas listas
+# c. A divisão dos elementos dessas listas
+
+q = np.array([1, 2, 3])
+r = np.array([4, 5, 6])
+print("19)a) ", q + r)
+print("19)b) ", q * r)
+print("19)c) ", q / r)
+
+# 20) Crie 1 array de 24 elementos e apresente:
+# a. O maior valor
+# b. O menor valor
+# c. A soma dos valores
+# d. A média dos valores
+# e. A soma de cada linha
+# f. A soma de cada coluna
+
+s = np.arange(24).reshape(4, 6)
+print("20) ", s)
+print("20)a) ", s.max())
+print("20)b) ", s.min())
+print("20)c) ", s.sum())
+print("20)d) ", s.mean())
+print("20)e) ", s.sum(axis = 1))
+print("20)f) ", s.sum(axis = 0))
+
+# 21) Crie na forma de polinômio usando o comando poly1d os seguintes polinômios:
+# a. 3x + 4
+# b. 4x^3 + 3x^2 + 2x + 1
+# c. 2x^3 + 3
+# d. x^2 + 2x + 3
+# e. Multiplique os polinômios c e d
+# f. Derive o polinômio d
+# g. Integre o polinômio d
+
+t = np.poly1d([3, 4])
+u = np.poly1d([4, 3, 2, 1])
+v = np.poly1d([2, 0, 0, 3])
+w = np.poly1d([1, 2, 3])
+x = v * w
+y = w.deriv()
+z = w.integ()
+print("21)a) ", t)
+print("21)b) \n", u)
+print("21)c) \n", v)
+print("21)d) \n", w)
+print("21)e) \n", x)
+print("21)f) \n", y)
+print("21)g) \n", z)
